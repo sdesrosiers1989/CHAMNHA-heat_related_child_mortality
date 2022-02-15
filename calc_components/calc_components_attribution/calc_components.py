1f370ac449ac35a0ef505fcd191586e8fc40052a
Sarah.Chapman <earsch@leeds.ac.uk>
2022-02-15 16:09:32 +0000

    Decompose components of change in HRCM using Das Gupta 1993 method, ver 6

--- calc_components/calc_components_attribution/calc_components.py
+++ calc_components/calc_components_attribution/calc_components.py
@@ -14,6 +14,8 @@ Created on Mon Apr 20 11:12:29 2020
 #%%set wd and import packages
 import matplotlib.pyplot as plt 
 import matplotlib.gridspec as gridspec
+from matplotlib.patches import Patch
+from matplotlib.lines import Line2D
 
 import iris
 import iris.plot as iplt
@@ -404,7 +406,218 @@ damip_p3 = cube_to_frame(plist3, mlist3, elist3, dif3, 'period3')
 his_p2 = cube_to_frame(plist_h, mlist_h, elist_h, dif_h, 'period2')
 his_p3 = cube_to_frame(plist_h3, mlist_h3, elist_h3, dif_h3, 'period3')
 
-damip_agg = damip_p2.mean()
-his_agg = his_p2.mean()
-damip_agg3 = damip_p3.mean()
-his_agg3 = his_p3.mean()
+damip = damip_p2.append(damip_p3)
+his = his_p2.append(his_p3)
+
+
+damip_agg = damip.groupby(['period'], as_index = True).mean()
+his_agg = his.groupby(['period'], as_index = True).mean()
+
+d_per = damip_agg.drop(columns = ['p' , 'm', 'e', 'total'])
+h_per = his_agg.drop(columns = ['p' , 'm', 'e', 'total'])
+
+d_abs = damip_agg.drop(columns = ['p_per' , 'm_per', 'e_per', 'total'])
+h_abs = his_agg.drop(columns = ['p_per' , 'm_per', 'e_per', 'total'])
+
+#otherwise looks like population growth makes mortlaity go down
+d_abs = d_abs * -1
+h_abs = h_abs * -1
+
+#%% https://stackoverflow.com/questions/22787209/how-to-have-clusters-of-stacked-bars-with-python-pandas
+
+def plot_clustered_stacked(dfall, cols, labels=None, title="",  H="/", width = 0.5,  **kwargs):
+    """Given a list of dataframes, with identical columns and index, create a clustered stacked bar plot. 
+labels is a list of the names of the dataframe, used for the legend
+title is a string for the title of the plot
+H is the hatch used for identification of the different dataframe"""
+
+    n_df = len(dfall)
+    n_col = len(dfall[0].columns) 
+    n_ind = len(dfall[0].index)
+    axe = plt.subplot(111)
+
+    for df in dfall : # for each data frame
+        axe = df.plot(kind="bar",
+                      linewidth=0,
+                      stacked=True,
+                      ax=axe,
+                      legend=False,
+                      grid=False,
+                      color = cols,
+                      **kwargs)  # make bar plots
+
+    h,l = axe.get_legend_handles_labels() # get the handles we want to modify
+    for i in range(0, n_df * n_col, n_col): # len(h) = n_col * n_df
+        for j, pa in enumerate(h[i:i+n_col]):
+            for rect in pa.patches: # for each index
+                rect.set_x(rect.get_x() + width / float(n_df + width) * i / float(n_col))
+                rect.set_hatch(H * int(i / n_col)) #edited part     
+                rect.set_width(width / float(n_df + width))
+
+    axe.set_xticks(np.arange(n_ind))
+    axe.set_xticklabels(df.index, rotation = 0)
+    #axe.set_title(title)
+
+    # Add invisible data to add another legend
+    #n=[]        
+    #for i in range(n_df):
+    #    n.append(axe.bar(0, 0, color="gray", hatch=H * i))
+
+    #l1 = axe.legend(h[:n_col], l[:n_col], loc=[1.01, 0.5])
+    #if labels is not None:
+     #   l2 = plt.legend(n, labels, loc=[1.01, 0.1]) 
+    #axe.add_artist(l1)
+    return axe
+
+# create fake dataframes
+df1 = pd.DataFrame(np.random.rand(4, 5),
+                   index=["A", "B", "C", "D"],
+                   columns=["I", "J", "K", "L", "M"])
+df2 = pd.DataFrame(np.random.rand(4, 5),
+                   index=["A", "B", "C", "D"],
+                   columns=["I", "J", "K", "L", "M"])
+df3 = pd.DataFrame(np.random.rand(4, 5),
+                   index=["A", "B", "C", "D"], 
+                   columns=["I", "J", "K", "L", "M"])
+
+# Then, just call :
+plot_clustered_stacked([df1, df2],labels = ["df1", "df2"], cols = ['black', 'green', 'blue'])
+
+#%% plot
+
+labels = ['Period 2', 'Period 3', '6', '7']
+mor_means = [his_agg['m_per'], damip_agg['m_per'], his_agg3['m_per'], damip_agg3['m_per']]
+pop_means = [his_agg['p_per'], damip_agg['p_per'], his_agg3['p_per'], damip_agg3['p_per']]
+e_means = [his_agg['e_per'], damip_agg['e_per'], his_agg3['e_per'], damip_agg3['e_per']]
+width  = 0.35
+
+plt.bar(labels, mor_means, width, label = 'Mortality')
+plt.bar(labels, pop_means, width, label = 'Population')
+plt.bar(labels, e_means, width, label = 'Climate')
+
+plt.legend()
+
+#%%
+
+plot_clustered_stacked([h_per, d_per],["his", "damip"])
+
+#%%
+ax = plot_clustered_stacked([h_abs, d_abs],labels = ["his", "damip"], cols = ['#1b9e77', '#7570b3', '#d95f02'])
+
+plt.xlabel('Period')
+
+ax.set_ylabel('Mean annual heat mortality per gridcell')
+ax.set_xticklabels(['2005 - 2014', '2015 - 2020'])
+
+ax.axhline(0, c = 'k', linewidth = 0.5)
+
+
+
+mor_patch = Patch(facecolor='#7570b3')
+pop_patch = Patch(facecolor='#1b9e77')
+e_patch = Patch(facecolor='#d95f02')
+his_patch = Patch(facecolor='grey')
+damip_patch = Patch(facecolor='grey', hatch = '//')
+
+custom_lines = [pop_patch, his_patch, mor_patch, damip_patch, e_patch]
+
+
+ax.legend(custom_lines, ['Population', 'Historical', 'Mortality', 
+                         'Hist-nat', 'Climate'], 
+           bbox_to_anchor=(0.9, -0.2),ncol=3,frameon = False, handletextpad = 0.5)
+
+
+
+
+#%%
+
+
+fig = plt.figure(figsize=(6,6))
+ax = fig.add_subplot(1,1,1)
+
+barWidth = 0.1
+
+# Set position of bar on X axis (position for each rcm bar within each gcm category)
+positions = []
+r1 = np.arange(2)
+positions.append(r1)
+
+periods = ['Period2', 'Period3']
+scen_dat = [h_abs, d_abs]
+
+for i in np.arange(1,len(periods)):
+    next_pos = [x + barWidth for x in positions[i-1]]
+    positions.append(next_pos)
+ 
+#set colors
+cols = ['green', 'blue', 'red']
+var_name = ['m', 'p', 'e']
+var_label = ['Mortality', 'Population', 'Climate']
+scen_label = ['hst', 'hist-nat']
+
+lw = 2.0
+
+
+# Make the plot
+for i in np.arange(len(scen_dat)):
+    pos = positions[i]
+    dat = scen_dat[i]
+    label = scen_label
+    plt.bar(pos, [5, 10], width = barWidth, color = cols[i], label = label)
+    plt.bar(pos, [5, 10], width = barWidth, color = cols[2], label = label)
+    
+    
+    #%%
+#ax.set_ylim([-70, 70])
+#ax.axhline(y = 0, color = 'k', linewidth = 0.5)
+#ax.annotate(s = var_name[j], xy = (0.01, 1.02), xycoords = 'axes fraction')
+# Add xticks on the middle of the group bars
+ax.set_xticks(positions[5]) #positions 5 is the middle rcm
+ax.set_xticklabels(np.repeat('', 11))
+
+#add lines between groups
+y = [25, 0]
+if j == 2: 
+    y = [0.025, 0]
+elif j == 1:
+    y = [15, 0]
+    
+adjust = 0.05
+for i in np.arange(0, len(positions[0])):
+    #position[0] is position of the first gcm
+    #position[-1] has the position of the last gcm
+    x1 = positions[0][i]
+    x = [x1 - adjust, x1 - adjust]     
+    ax.plot(x, y, color = 'black', linewidth = 0.5)
+
+
+if j == 2:
+    ax.set_xlabel('RCM')
+    ax.set_xticklabels(rcm_short)
+ax.tick_params(bottom = 'False')
+
+ax.set_ylabel(var_label[j])
+    
+    # add CP4 and P25
+    ax.scatter(7.3, df_cp4[var_name[j].lower()][(df_cp4['scen'] == 'hist')].values, c = 'black',
+                            label = 'CP4A')
+    ax.scatter(7.3, df_p25[var_name[j].lower()][(df_p25['scen'] == 'hist')].values, c = 'black',
+                            marker = 's', label = 'P25')
+    #add trmm and obs
+    
+    ax.axhline(df15_agg_obs[var_name[j].lower()][(df15_agg_obs['mod'] == 'obs_trmm')].values,
+                            linestyle = 'dotted', color = '#e52929', label = 'TRMM', linewidth = lw)
+                            
+    ax.axhline(df38_agg_obs[var_name[j].lower()].values,
+                             color = '#e52929', label = 'CHIRPS', linewidth = lw)
+    
+
+plt.draw()
+#plt.annotate('Change in rainfall (mm/month)', (0.04, 0.65), xycoords = 'figure fraction',
+#             rotation = 90)
+
+# Create legend & Show graphic
+plt.legend(loc='upper center', bbox_to_anchor=(0.5, -0.25),ncol=4, frameon = False)
+
+#fig.savefig('/nfs/see-fs-02_users/earsch/Documents/Leeds/Barchar_params_30_rcms_obs.png',
+#            bbox_inches = 'tight', pad_inches = 0.3)
