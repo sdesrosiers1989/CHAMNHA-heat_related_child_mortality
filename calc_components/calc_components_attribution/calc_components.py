1fea6c8012cfcd4e784ad8b477b879f80951eccf
Sarah.Chapman <earsch@leeds.ac.uk>
2022-02-15 16:08:30 +0000

    Decompose components of change in HRCM using Das Gupta 1993 method, ver 3

--- calc_components/calc_components_attribution/calc_components.py
+++ calc_components/calc_components_attribution/calc_components.py
@@ -171,34 +171,25 @@ def calc_comp(cube_list, return_list =  False):
 
 
 #%% Import outputs of ifnant mortality health burden model
+    # compare hist-nat T1 and T2
     
 codes = ['e2p1m1', 'e2p2m1', 'e2p1m2',
         'e1p1m1', 'e1p2m1', 'e1p1m2',
         'e2p2m2', 'e1p2m2']
 
-path = '/nfs/a321/earsch/CHAMNHA/output/annual_avg_mortality/coeff_061/thres_hismodel/decomp_attribution/'
-
+path = '/nfs/a321/earsch/CHAMNHA/output/annual_avg_mortality/coeff_061/thres_hismodel/decomp_attribution2/2010/'
+damip_mods = []
 filenames = glob.glob(path + '*.nc')
 cube_list = iris.cube.CubeList()
 for file in filenames:
     x = iris.load_cube(file, callback = add_code_from_filename)
-    cube_list.append(x)
-
-
-#'e2p2m2', 'e1p2m2'
-    #hist-nat = e1
-    #hist = e2
-path = '/nfs/a321/earsch/CHAMNHA/output/annual_avg_mortality/coeff_061/thres_hismodel/'
-filenames = glob.glob(path + '*hist-nat*.nc')
-damip_mods = []
-for file in filenames:
-    x = iris.load_cube(file)
-    x.add_aux_coord(iris.coords.AuxCoord('e1p2m2', long_name = 'code'))
     damip_mods.append(tp.gcm(x))
     cube_list.append(x)
 
 
-filenames = glob.glob(path + '*historical*.nc')
+#'e2p2m2'
+path = '/nfs/a321/earsch/CHAMNHA/output/annual_avg_mortality/coeff_061/thres_hismodel/'
+filenames = glob.glob(path + '*hist-nat*20052014*.nc')
 for file in filenames:
     x = iris.load_cube(file)
     x.add_aux_coord(iris.coords.AuxCoord('e2p2m2', long_name = 'code'))
@@ -211,198 +202,64 @@ for code in codes:
     new_list = [x for x in cube_list if x.coord('code').points == code]
     code_cubes.append(new_list)
     
-    
-#%% Split into years
-    
-years = np.unique([x.coord('year').points[0] for x in cube_list]) 
-
-#code_years = []
-#for cube_list in code_cubes:
-#    new_list = iris.cube.CubeList()
-#    for y in years:
-#        newy_list = [x for x in cube_list if x.coord('year').points[0] == y]
-#    
-#        new_list.append(newy_list)
-#    code_years.append(new_list)
-    
-code_years = iris.cube.CubeList()
-for y in years:
-    new_list = iris.cube.CubeList()
-    for cube_list in code_cubes:
-        newy_list = [x for x in cube_list if x.coord('year').points[0] == y]
-
-        new_list.append(newy_list)
-    code_years.append(new_list)
-    
-#%% What is the cause of the differences between hist and hist-nat?
+        
+#%% What is the cause of the differences between hist-nat T1 and T2?
     
 #1995 - 2004: pop and mortality the same (onyl due to climate)
-period1 = code_years[0]
-hist_period1 = period1[6]
-histnat_period1 = period1[7]
+period1 = [x for x in code_cubes if x[0].coord('code').points == 'e1p1m1'][0]
+period2 = [x for x in code_cubes if x[0].coord('code').points == 'e2p2m2'][0]
 
-dif, dif_per = find_dif(hist_period1, histnat_period1)
+dif, dif_per = find_dif(period1, period2)
 
 ens_dif = ens_mean(dif)
 ens_perdif = ens_mean(dif_per)
 
-#second period comparison: 
-period2 = code_years[1]
-hist_period2 = period2[6]
-histnat_period2 = period2[7]
+period1_ens = ens_mean(period1)
+period2_ens = ens_mean(period2)
 
-dif2, dif_per2 = find_dif(hist_period2, histnat_period1)
 
-ens_dif2 = ens_mean(dif2)
-ens_perdif2 = ens_mean(dif_per2)
-
-#%% Cause of difference between CCPeriod2 and HISTNATPeriod1/2?
+#%% Cause of difference between Histnat T1 and T2?
     
 #codes = ['e2p1m1', 'e2p2m1', 'e2p1m2',
 #        'e1p1m1', 'e1p2m1', 'e1p1m2',
 #        'e2p2m2', 'e1p2m2']
 
-p_effect2005, m_effect2005, e_effect2005 = calc_comp(code_years[1])
+p_effect, m_effect, e_effect, plist, mlist, elist = calc_comp(code_cubes, return_list = True)
     
 #%%
+print(np.nanmean(period1_ens.data))
+print(np.nanmean(period2_ens.data))
+print(np.nanmean(ens_dif.data))
 
-np.nanmean(ens_dif2.data)
 
--1 * np.nanmean(p_effect2005.data) + np.nanmean(m_effect2005.data) +  (np.nanmean(e_effect2005.data) * -1)
+np.nanmean(p_effect.data)
+np.nanmean(m_effect.data)
+np.nanmean(e_effect.data)
 
-#%% Get ens mean
-
-ens_hist = []
-ens_damip = []
-ens_damip_ht = []
-
-for i in np.arange(len(damip_years)):
-    
-    x = ens_mean(damip_years[i])
-    ens_damip.append(x)
-    
-    x = ens_mean(damip_ht_years[i])
-    ens_damip_ht.append(x)
-    
-    x = ens_mean(his_years[i])
-    ens_hist.append(x)
-    
-
-ens_dif = [ens_mean(x) for x in dif_list]
-ens_dif_ht = [ens_mean(x) for x in dif_ht_list]
-
-#coeff 1
-ens_hist1 = []
-ens_damip1 = []
-ens_damip1_ht = []
-
-for i in np.arange(len(damip_years1)):
-    
-    x = ens_mean(damip_years1[i])
-    ens_damip1.append(x)
-    
-    x = ens_mean(damip_ht_years1[i])
-    ens_damip1_ht.append(x)
-    
-    x = ens_mean(his_years1[i])
-    ens_hist1.append(x)
-    
-
-ens_dif1 = [ens_mean(x) for x in dif_list1]
-ens_dif1_ht = [ens_mean(x) for x in dif_ht_list1]
+np.nanmean(p_effect.data) + np.nanmean(m_effect.data) +  np.nanmean(e_effect.data) 
 
 
 #%%
-    
-    #coeff 0.61
-cube_list = [ens_hist[0], ens_hist[1], ens_hist[2], 
-             ens_damip_ht[0], ens_damip_ht[1], ens_damip_ht[2],
-             ens_dif_ht[0], ens_dif_ht[1], ens_dif_ht[2]]
+
+i, j, k = 40,40, 0 
+
+print(period1[k][i,j].data)
+print(period2[k][i,j].data)
+print(dif[k][i,j].data)
 
 
-# coeff 1
-#cube_list = [ens_hist1[0], ens_hist1[1], ens_hist1[2], 
-#             ens_damip1[0], ens_damip1[1], ens_damip1[2],
-#             ens_dif1[0], ens_dif1[1], ens_dif1[2]]
+print(plist[k][i,j].data)
+print(mlist[k][i,j].data)
+print(elist[k][i,j].data)
 
+plist[k][i,j].data + mlist[k][i,j].data +  elist[k][i,j].data
 
-rows = 3
-cols = 3
-nfigs = rows * cols
+#%% What percentage of the change is due to climate, pop and mortality?
 
-no_grids = np.arange(1,nfigs - cols)[np.mod(np.arange(1, nfigs - cols),cols)!= 0]
-y_only = np.arange(0, nfigs - cols, cols)
+total_change = np.nanmean(ens_dif.data)
 
-levels = np.arange(0, 55, 5)
-levs_dif = np.arange(-5, 6, 1)
-#levels_dif = np.arange(-200, 250, 50)
-xticks = np.arange(-10, 60, 20)
-yticks = [40, 20, 0, -20, -40]
-
-fig = plt.figure(figsize=(9,10))
-
-ax_list = []
-for i in np.arange(1, nfigs+1):
-    ax_list.append(plt.subplot(rows, cols, i, projection = proj))
-
-for i in np.arange(0, len(cube_list)):
-    ax = ax_list[i]
-    cube = cube_list[i]
-    
-    if i < 6:
-        im = iplt.contourf(cube, axes = ax, cmap = 'YlOrRd', levels = levels, extend = 'max')
-    else:
-        im_dif = iplt.contourf(cube, axes = ax, cmap = 'RdBu_r', levels = levs_dif, extend = 'both')
-    
-    if i in no_grids:
-        tp.plot_africa(ax_list[i], high = False, no_x = True, no_y = True, Tanga = False)
-    elif i in y_only:
-        tp.plot_africa(ax_list[i], high = False, no_x = True, yticks = yticks, Tanga = False)
-    elif i == no_grids[-1] + 1:
-        tp.plot_africa(ax_list[i], high = False, xticks = xticks, yticks = yticks, Tanga = False)
-    else:
-        tp.plot_africa(ax_list[i], high = False, no_y = True, xticks = xticks, Tanga = False)
-
-       
-    #ax.set_title(title)
-    #tp.plot_africa(ax, high = False, xticks = xticks)
-
-xpos = 0.5
-ypos = 1.05
-ax_list[0].annotate(s = '1995 - 2004', xy = (xpos, ypos), xycoords = 'axes fraction',
-             ha = 'center')
-ax_list[1].annotate(s = '2005 - 2014', xy = (xpos, ypos), xycoords = 'axes fraction',
-             ha = 'center')
-ax_list[2].annotate(s = '2015 - 2020', xy = (xpos, ypos), xycoords = 'axes fraction',
-             ha = 'center')
-
-
-xpos = -0.35
-ypos = 0.5
-ax_list[0].annotate(s = 'Historical', xy = (xpos, ypos), xycoords = 'axes fraction',
-             va = 'center', rotation = 90)
-ax_list[3].annotate(s = 'Hist-nat', xy = (xpos, ypos), xycoords = 'axes fraction',
-             va = 'center', rotation = 90)
-ax_list[6].annotate(s = 'Historical minus his-nat', xy = (xpos, ypos), xycoords = 'axes fraction',
-             va = 'center', rotation = 90)
-
-
-plt.draw()
-
-dif = 0.05
-
-cbax = tp.get_cbax(fig, ax = ax_list[3], last_ax = [ax_list[5]], orientation = 'horizontal', 
-                   dif = dif/2, h_w = 0.02)
-cbar = plt.colorbar(im, cax = cbax, orientation = 'horizontal')
-cbar.set_label('Annual avg. heat related deaths for 0 - 4 year olds ')
-
-cbax = tp.get_cbax(fig, ax = ax_list[6], last_ax = [ax_list[-1]], orientation = 'horizontal', 
-                   dif = dif, h_w = 0.02)
-cbar = plt.colorbar(im_dif, cax = cbax, orientation = 'horizontal')
-cbar.set_label('Difference in annual avg. heat related deaths for 0 - 4 year olds ')
-
-#
-#fig.savefig('/nfs/see-fs-02_users/earsch/Documents/Leeds/attributionresulst_ensmean_difens_coeff061_threshismodel.png',
-#         bbox_inches = 'tight', pad_inches = 0.3)
+p_per = np.nanmean(p_effect.data) / total_change * 100
+m_per = np.nanmean(m_effect.data) / total_change * 100
+e_per = np.nanmean(e_effect.data) / total_change * 100
 
 
