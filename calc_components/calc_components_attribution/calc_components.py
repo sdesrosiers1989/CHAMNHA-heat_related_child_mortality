f8533f87d6c70dfbc10a2056a185c658e0a0e27d
Sarah.Chapman <earsch@leeds.ac.uk>
2022-02-15 16:08:59 +0000

    Decompose components of change in HRCM using Das Gupta 1993 method, ver 4

--- calc_components/calc_components_attribution/calc_components.py
+++ calc_components/calc_components_attribution/calc_components.py
@@ -42,19 +42,12 @@ import sys
 sys.path.append('/nfs/see-fs-02_users/earsch/Documents/Leeds/Repos/Tanga/Plot_functions')
 import tanzania1 as tp
 
-def add_code_from_filename(cube, field, filename):
-    #split filename into sections by '_', find second partition and split again
-    file = filename[97:-3]
-    
-    code = file.partition('_')[2].partition('_')[2].partition('_')[2]
-
-    cube.add_aux_coord(iris.coords.AuxCoord(code, long_name = 'code'))
-
 def ens_mean(cube_list):  
     cor_cubes = iris.cube.CubeList()
     #add ensemble as dimension so can use cube statistics
-    for item in cube_list:
-        gcm = item.coord('gcm').points[0]
+    for i in np.arange(len(cube_list)):
+        item = cube_list[i]
+        gcm = i
         try:
             item.add_aux_coord(iris.coords.AuxCoord(gcm, long_name = 'ens'))
         except:
@@ -98,116 +91,172 @@ def find_dif(his_list, histnat_list):
 
 #%%
     
-def calc_comp(cube_list, return_list =  False):
+def calc_comp(p_list, m_list, e_list, return_list =  False):
     '''order should be:
-        'e2p1m1', 'e2p2m1', 'e2p1m2',
-        'e1p1m1', 'e1p2m1', 'e1p1m2'
-        'e2p2m2', 'e1p2m2'
+    period1, period2
+    list of models within each period only for e (pop and mor same each model)
     '''
     
-    p1m1e1_list = cube_list[3]
-    p1m2e2_list = cube_list[2]
-    p1m1e2_list = cube_list[0]
-    p1m2e1_list = cube_list[5]
-    p2m1e1_list = cube_list[4]
-    p2m2e2_list = cube_list[6]
-    p2m1e2_list = cube_list[1]
-    p2m2e1_list = cube_list[7]
+    p1 = p_list[0].data
+    p2 = p_list[1].data
+    m1 = m_list[0].data
+    m2 = m_list[1].data 
+    e1_mlist = e_list[0]
+    e2_mlist = e_list[1]
     
-    p_list = []
-    m_list = []
-    e_list = []
+    p_outlist = iris.cube.CubeList()
+    m_outlist = iris.cube.CubeList()
+    e_outlist = iris.cube.CubeList()
     
-    for i in np.arange(len(p1m1e1_list)):
+    for i in np.arange(len(e1_mlist)):
         
-        p1m1e1 = p1m1e1_list[i]
+        e1 = e1_mlist[i].data
         
         #find same gcm
-        gcm = tp.gcm(p1m1e1)
+        gcm = tp.gcm(e1_mlist[i])
+        e2 = [x for x in e2_mlist if tp.gcm(x) == gcm][0].data
         
-        p1m2e2 = [x for x in p1m2e2_list if tp.gcm(x) == gcm][0]
-        p1m1e2 = [x for x in p1m1e2_list if tp.gcm(x) == gcm][0]
-        p1m2e1 = [x for x in p1m2e1_list if tp.gcm(x) == gcm][0]
-        p2m1e1 = [x for x in p2m1e1_list if tp.gcm(x) == gcm][0]
-        p2m2e2 = [x for x in p2m2e2_list if tp.gcm(x) == gcm][0]
-        p2m1e2 = [x for x in p2m1e2_list if tp.gcm(x) == gcm][0]
-        p2m2e1 = [x for x in p2m2e1_list if tp.gcm(x) == gcm][0]
-    
-        peq1 = (p1m1e1 + p1m2e2) / 3
-        peq2 = (p1m1e2 + p1m2e1) / 6
-        peq3 = (p2m1e1 + p2m2e2) / 3
-        peq4 = (p2m1e2 + p2m2e1) / 6
         
-        p_effect = (peq1 + peq2) - (peq3 + peq4)
+        peq1 = (m1 * e1 + m2 * e2)  / 3
+        peq2 = (m1 * e2 + m2 * e1) / 6
         
-        meq1 = (p1m1e1 + p2m1e2) / 3
-        meq2 = (p1m1e2 + p2m1e1) / 6
-        meq3 = (p1m2e1 + p2m2e2) / 3
-        meq4 = (p1m2e2 + p2m2e1) / 6
+        p_effect = copy.deepcopy(p_list[0])
+        p_effect.data = (p1 - p2)*(peq1 + peq2)
         
-        m_effect = (meq1 + meq2) - (meq3 + meq4)
+        meq1 = (p1 * e1 + p2 * e2)  / 3
+        meq2 = (p1 * e2 + p2 * e1) / 6
+                
+        m_effect = copy.deepcopy(m_list[0])
+        m_effect.data = (m1 - m2)*(meq1 + meq2)
         
-        eeq1 = (p1m1e1 + p2m2e1) / 3
-        eeq2 = (p1m2e1 + p2m1e1) / 6
-        eeq3 = (p1m1e2 + p2m2e2) / 3
-        eeq4 = (p1m2e2 + p2m1e2) / 6
+        eeq1 = (m1 * p1 + m2 * p2)  / 3
+        eeq2 = (p1 * m2 + p2 * m1) / 6
+                
+        e_effect = copy.deepcopy(e1_mlist[i])
+        e_effect.data = (e1 - e2)*(eeq1 + eeq2)
         
-        e_effect = (eeq1 + eeq2) - (eeq3 + eeq4)
+        p_outlist.append(p_effect)
+        m_outlist.append(m_effect)
+        e_outlist.append(e_effect)
         
-        p_list.append(p_effect)
-        m_list.append(m_effect)
-        e_list.append(e_effect)
-        
-    p_ens = ens_mean(p_list)
-    m_ens = ens_mean(m_list)
-    e_ens = ens_mean(e_list)
+    p_ens = ens_mean(p_outlist)
+    m_ens = ens_mean(m_outlist)
+    e_ens = ens_mean(e_outlist)
         
     if return_list == True:
-        return p_ens, m_ens, e_ens, p_list, m_list, e_list
+        return p_ens, m_ens, e_ens, p_outlist, m_outlist, e_outlist
     else:
         return p_ens, m_ens, e_ens
     #return p_ens, m_ens, e_ens, p_list, m_list, e_list
 
-
-
-#%% Import outputs of ifnant mortality health burden model
-    # compare hist-nat T1 and T2
+#%% import e model data
     
-codes = ['e2p1m1', 'e2p2m1', 'e2p1m2',
-        'e1p1m1', 'e1p2m1', 'e1p1m2',
-        'e2p2m2', 'e1p2m2']
+path = '/nfs/a321/earsch/CHAMNHA/output/e/coeff_061/historical/'
 
-path = '/nfs/a321/earsch/CHAMNHA/output/annual_avg_mortality/coeff_061/thres_hismodel/decomp_attribution2/2010/'
+#hist-nat
 damip_mods = []
-filenames = glob.glob(path + '*.nc')
-cube_list = iris.cube.CubeList()
-for file in filenames:
-    x = iris.load_cube(file, callback = add_code_from_filename)
-    damip_mods.append(tp.gcm(x))
-    cube_list.append(x)
-
-
-#'e2p2m2'
-path = '/nfs/a321/earsch/CHAMNHA/output/annual_avg_mortality/coeff_061/thres_hismodel/'
-filenames = glob.glob(path + '*hist-nat*20052014*.nc')
+filenames = glob.glob(path + '*hist-nat*.nc')
+damip_list = iris.cube.CubeList()
 for file in filenames:
     x = iris.load_cube(file)
-    x.add_aux_coord(iris.coords.AuxCoord('e2p2m2', long_name = 'code'))
+    damip_mods.append(tp.gcm(x))
+    damip_list.append(x)
+
+
+#historical
+filenames = glob.glob(path + '*historical*.nc')
+hist_list = iris.cube.CubeList()
+for file in filenames:
+    x = iris.load_cube(file)
     if tp.gcm(x) in damip_mods:
-        cube_list.append(x)
+        hist_list.append(x)
+  
+#%% Import pop and mor data 
+
+#population -from world pop
+# 0 - 4 year olds
+pop_2000 = iris.load_cube('/nfs/a321/earsch/CHAMNHA/input_data/pop/processed/afr_01_mf_2000_regrid.nc')
+pop_2010 = iris.load_cube('/nfs/a321/earsch/CHAMNHA/input_data/pop/processed/afr_01_mf_2010_regrid.nc')
+pop_2019 = iris.load_cube('/nfs/a321/earsch/CHAMNHA/input_data/pop/processed/afr_01_mf_2019_regrid.nc')
+
+#daily mortality
+dmor_2000 = iris.load_cube('/nfs/a321/earsch/CHAMNHA/input_data/mortality/processed/daily_mor_mf_01_2000_regrid.nc')
+dmor_2010 = iris.load_cube('/nfs/a321/earsch/CHAMNHA/input_data/mortality/processed/daily_mor_mf_01_2010_regrid.nc')
+dmor_2019 = iris.load_cube('/nfs/a321/earsch/CHAMNHA/input_data/mortality/processed/daily_mor_mf_01_2019_regrid.nc')
 
 
-code_cubes = []
-for code in codes:
-    new_list = [x for x in cube_list if x.coord('code').points == code]
-    code_cubes.append(new_list)
+pop_list = [pop_2000, pop_2010, pop_2019]
+mor_list = [dmor_2000, dmor_2010, dmor_2019]
+
+#%% actual mortality
+
+path = '/nfs/a321/earsch/CHAMNHA/output/annual_avg_mortality/coeff_061/thres_hismodel/'
+
+filenames = glob.glob(path + '*hist-nat*')
+damip_mor = iris.cube.CubeList()
+for file in filenames:
+    x = iris.load_cube(file)
+    damip_mor.append(x)
+
+#%% calc pop ratio as sued in health burden model
+#pop data
+
+pop_ratio_list = []
+for i in np.arange(len(pop_list)):
+
+    pop_ratio = pop_list[i]/pop_2000 #ratio future pop to baseline pop   
+    #0 in denominator causing problems
+    pop_ratio.data = np.where(pop_2000.data == 0, 1, pop_ratio.data)
+    pop_ratio.data = ma.masked_array(pop_ratio.data, mask = pop_2000.data.mask)  
+    pop_ratio_list.append(pop_ratio)
     
-        
-#%% What is the cause of the differences between hist-nat T1 and T2?
+#%% get mdoels into year lists
     
+years = np.unique([x.coord('year').points[0] for x in hist_list]) #damip and his years same
+
+damip_years =  iris.cube.CubeList()
+his_years = iris.cube.CubeList()
+
+for y in years:
+    d_list = [x for x in damip_list if x.coord('year').points[0] == y]
+    d_mean_list = iris.cube.CubeList()
+    for cube in d_list:
+        d_mean_list.append(cube.collapsed('time', iris.analysis.MEAN))
+
+    h_list = [x for x in hist_list if x.coord('year').points[0] == y]
+    h_mean_list = iris.cube.CubeList()
+    for cube in h_list:
+        h_mean_list.append(cube.collapsed('time', iris.analysis.MEAN))
+    
+    damip_years.append(d_mean_list)
+    his_years.append(h_mean_list)
+    
+#%% mortality years
+
+years = np.unique([x.coord('year').points[0] for x in damip_mor]) #damip and his years same
+
+damip_mor_years =  iris.cube.CubeList()
+
+for y in years:
+    d_list = [x for x in damip_mor if x.coord('year').points[0] == y]
+
+    
+    damip_mor_years.append(d_list)
+
+
+#%% Cause of difference between Histnat T1 and T2?
+    
+p_inlist = [pop_ratio_list[0], pop_ratio_list[1]]
+m_inlist = [dmor_2000, dmor_2010]
+e_inlist = [damip_years[0], damip_years[1]]
+
+p_effect, m_effect, e_effect, plist, mlist, elist = calc_comp(p_inlist, m_inlist, e_inlist, return_list = True)
+    
+#%% Check against actual dif
+
 #1995 - 2004: pop and mortality the same (onyl due to climate)
-period1 = [x for x in code_cubes if x[0].coord('code').points == 'e1p1m1'][0]
-period2 = [x for x in code_cubes if x[0].coord('code').points == 'e2p2m2'][0]
+period1 = damip_mor_years[0]
+period2 = damip_mor_years[1]
 
 dif, dif_per = find_dif(period1, period2)
 
@@ -217,26 +266,17 @@ ens_perdif = ens_mean(dif_per)
 period1_ens = ens_mean(period1)
 period2_ens = ens_mean(period2)
 
-
-#%% Cause of difference between Histnat T1 and T2?
-    
-#codes = ['e2p1m1', 'e2p2m1', 'e2p1m2',
-#        'e1p1m1', 'e1p2m1', 'e1p1m2',
-#        'e2p2m2', 'e1p2m2']
-
-p_effect, m_effect, e_effect, plist, mlist, elist = calc_comp(code_cubes, return_list = True)
-    
 #%%
+
 print(np.nanmean(period1_ens.data))
 print(np.nanmean(period2_ens.data))
 print(np.nanmean(ens_dif.data))
-
 
 np.nanmean(p_effect.data)
 np.nanmean(m_effect.data)
 np.nanmean(e_effect.data)
 
-np.nanmean(p_effect.data) + np.nanmean(m_effect.data) +  np.nanmean(e_effect.data) 
+np.nanmean(p_effect.data) +  np.nanmean(m_effect.data) +   np.nanmean(e_effect.data) 
 
 
 #%%
@@ -246,7 +286,6 @@ i, j, k = 40,40, 0
 print(period1[k][i,j].data)
 print(period2[k][i,j].data)
 print(dif[k][i,j].data)
-
 
 print(plist[k][i,j].data)
 print(mlist[k][i,j].data)
