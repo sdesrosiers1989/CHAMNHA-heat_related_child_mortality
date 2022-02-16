9993925408d9ddcb2d994c389b96fa8609734bfb
Sarah.Chapman <earsch@leeds.ac.uk>
2022-02-16 12:06:13 +0000

    Prepare GBD Ref scenario mortality data for use in health burden model

---
+++ prepare_popmor_data/future/mortality/prepare_future_mor_data.py
@@ -0,0 +1,141 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Process SSP future data for use
+
+-in csv format
+-link to coutnry raster(dictionary)
+-distribution using 2019 pop distribution
+
+Created on Tue Aug 17 11:44:49 2021
+
+@author: earsch
+"""
+#%% import libraris
+
+import iris
+import iris.quickplot as qplt
+
+import matplotlib.pyplot as plt
+
+import glob
+
+import numpy as np
+
+import pandas as pd
+
+import copy
+#%%
+
+countries = iris.load_cube('/nfs/a321/earsch/CHAMNHA/input_data/africa_countryname.nc')
+popfrac_2019 = iris.load_cube('/nfs/a321/earsch/CHAMNHA/input_data/pop/popfrac_2019_regrid.nc')
+pop2019 = iris.load_cube('/nfs/a321/earsch/CHAMNHA/input_data/pop/processed/afr_01_mf_2019_regrid.nc')
+
+
+country_lookup = pd.read_csv('/nfs/a321/earsch/CHAMNHA/input_data/pop/country_lookup.csv')
+
+mor_2019 = iris.load_cube('/nfs/a321/earsch/CHAMNHA/input_data/mortality/processed/total_mor_mf_01_2019_regrid.nc')
+
+
+#%% Create diciotnary of country codes and names
+
+c_dict = dict(zip(country_lookup['Value'], country_lookup['Name']))
+
+
+#%% regrid countries to poprfac
+
+countries_regrid = countries.regrid(popfrac_2019, iris.analysis.Nearest())
+
+
+#%% GBD future mortality
+
+total_mor_2019 = np.nansum(mor_2019.data) / 1000 # GBD in 100s
+
+d2016 = 54698.58
+
+year = np.array([2016, 2040, 2040, 2040])
+scen = np.array(['his', 'ref', 'better', 'worse'])
+deaths = np.array([54698.58, 75263.26, 62570.88, 96717.00])
+deaths_per_2016 = deaths/d2016
+
+deaths_df = pd.DataFrame(data = {'year': year,
+                                 'scen': scen,
+                                 'deaths': deaths,
+                                 'deaths_per_2016':deaths_per_2016})
+
+
+#%% erplace country code with total under 5 population
+
+save_path = '/nfs/a321/earsch/CHAMNHA/input_data/pop/future/processed/'
+
+def distr_pop(pop_table, scenario_name):
+
+
+    years = np.unique(pop_table['Year'])
+    years = years[years >= 2020]
+    
+    pop_output = []
+    
+    for y in years:
+    
+        year_dat = pop_table[pop_table['Year'] == y]
+        cnames = np.unique(year_dat['Area'])
+        
+        pop_year = copy.deepcopy(countries_regrid)
+        
+        # get all country codes from map
+        dat = pop_year.data
+        vals = np.unique(dat)
+        vals = vals[vals.mask == False]
+        
+        for val in vals:
+            c_name = c_dict[val]
+            print(c_name)
+            
+            if c_name in cnames:
+        
+                replace_val = year_dat[year_dat['Area'] == c_name]['Population'].values
+                print(replace_val)
+                
+                pop_year.data = np.where(pop_year.data == val, replace_val, pop_year.data)
+                
+                pop_year_frac = copy.deepcopy(pop_year)
+                pop_year_frac.data = pop_year_frac.data*popfrac_2019.data
+                
+        save_name = scenario_name + '_' + str(y) + '_04population_mf.nc'
+        iris.save(pop_year_frac, save_path + save_name)
+        
+        pop_output.append(pop_year_frac)
+    return pop_output
+
+pop_output = distr_pop(ssp2, 'ssp2')
+pop_output_ssp3 = distr_pop(ssp3, 'ssp3')
+
+#%%  check
+        
+pop_levs = [0, 3000, 20000, 30000, 50000, 100000, 250000, 500000] 
+
+qplt.contourf(pop2019, levels = pop_levs, extend = 'max', cmap = 'YlOrRd')
+qplt.contourf(pop_output_ssp3[0], levels = pop_levs, extend = 'max', cmap = 'YlOrRd')
+
+#%%
+print(np.nanmean(pop2019.data))
+mpop = [np.nanmean(x.data) for x in pop_output]
+mpop3 = [np.nanmean(x.data) for x in pop_output_ssp3]
+
+
+print(np.nansum(pop2019.data))
+tpop = [np.nansum(x.data) for x in pop_output]
+tpop3 = [np.nansum(x.data) for x in pop_output_ssp3]
+
+#%%
+
+years = np.unique(ssp2['Year'])
+years = years[years >= 2020]
+
+plt.plot(years, tpop, label = 'SSP2')
+plt.plot(years, tpop3, label = 'SSP3')
+plt.scatter(2019, np.nansum(pop2019.data), label = 'actual 2019')
+plt.xlabel('Year')
+plt.ylabel('Total African population (under 5)')
+plt.legend()
