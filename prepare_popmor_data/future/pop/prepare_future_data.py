d2ceefab88a280b3144a85ccd1af1b4881fafb15
Sarah.Chapman <earsch@leeds.ac.uk>
2022-02-16 12:16:15 +0000

    Prepare SSP2 future population data for use in health burden model, ver 6

--- prepare_popmor_data/future/pop/prepare_future_data.py
+++ prepare_popmor_data/future/pop/prepare_future_data.py
@@ -7,6 +7,9 @@ Process SSP future data for use
 -link to coutnry raster(dictionary)
 -distribution using 2019 pop distribution
 
+Biascorrect all countries based on specific country correction factor
+-need to be regridded and correctly masked prior to correction
+
 Created on Tue Aug 17 11:44:49 2021
 
 @author: earsch
@@ -36,15 +39,12 @@ pop2000 = iris.load_cube('/nfs/a321/earsch/CHAMNHA/input_data/pop/processed/afr_
 
 
 country_lookup = pd.read_csv('/nfs/a321/earsch/CHAMNHA/input_data/pop/country_lookup.csv')
-ssp2 = pd.read_csv('/nfs/a321/earsch/CHAMNHA/input_data/pop/future/ssp2_allafricacountries_04_bothsexes.csv', 
+ssp2 = pd.read_csv('/nfs/a321/earsch/CHAMNHA/input_data/pop/future/ssp2_allcountries_04_bothsexes.csv', 
                    skiprows = 8)
 
 ssp2['Population'] = ssp2['Population'] * 1000
 
-ssp3 = pd.read_csv('/nfs/a321/earsch/CHAMNHA/input_data/pop/future/ssp3_africa_04_bothsexes.csv', 
-                   skiprows = 8)
 
-ssp3['Population'] = ssp3['Population'] * 1000
 #%% Create diciotnary of country codes and names
 
 c_dict = dict(zip(country_lookup['Value'], country_lookup['Name']))
@@ -61,10 +61,16 @@ popfrac_2019.coord('latitude').guess_bounds()
 popfrac_2019.coord('longitude').guess_bounds()
 p2000_regrid = pop2000.regrid(popfrac_2019, iris.analysis.AreaWeighted())
 p2010_regrid = pop2010.regrid(popfrac_2019, iris.analysis.AreaWeighted())
-
+p2019_regrid = pop2019.regrid(popfrac_2019, iris.analysis.AreaWeighted())
 
 print(np.nansum(pop2000.data), np.nansum(p2000_regrid.data))
 print(np.nansum(pop2010.data), np.nansum(p2010_regrid.data))
+
+#%%apply countries mask to pop data
+
+p2019_regrid.data = np.ma.masked_array(p2019_regrid.data, countries_regrid.data.mask)
+p2010_regrid.data = np.ma.masked_array(p2010_regrid.data, countries_regrid.data.mask)
+p2000_regrid.data = np.ma.masked_array(p2000_regrid.data, countries_regrid.data.mask)
 
 
 #%% Bias correct -finding dif between 2000, 2010 - avg diff
@@ -99,6 +105,7 @@ def country_total(pop, country_raster):
     
 df_2000 = country_total(p2000_regrid, countries_regrid)
 df_2010 = country_total(p2010_regrid, countries_regrid)
+df_2019 = country_total(p2019_regrid, countries_regrid)
 
 df_ssp_2000 = ssp2[ssp2['Year'] == 2000]
 
@@ -113,11 +120,41 @@ both2010['dif'] = both2010['pop'] - both2010['Population']
 both_years = pd.merge(both2000, both2010, on = ['Area'])
 both_years['avg_dif'] = (both_years['dif_x'] + both_years['dif_y']) / 2
 
+x = country_total(popfrac_2019, countries_regrid)
+
+#%% recreate popfraoc
+
+country_totalpop = country_total(p2019_regrid, countries_regrid)
+cnames = country_totalpop['Area'].values
+   
+popfrac_2019 = copy.deepcopy(p2019_regrid)
+dims = popfrac_2019.shape    
+
+# get all country codes from map
+dat = popfrac_2019.data
+vals = np.unique(dat)
+vals = vals[vals.mask == False]
+
+        
+for i in np.arange(dims[0]):
+    for j in np.arange(dims[1]):
+        
+        val = countries_regrid[i,j].data
+        if np.ma.is_masked(val) == False:
+            c_name = c_dict[int(val)]
+            if c_name in cnames:
+    
+    
+                p = country_totalpop[country_totalpop['Area'] == c_name]['pop'].values
+                replace_val = p2019_regrid[i,j].data / p
+                popfrac_2019.data[i,j] = replace_val
+
+
 #%% erplace country code with total under 5 population
 
 save_path = '/nfs/a321/earsch/CHAMNHA/input_data/pop/future/processed/'
 
-def distr_pop(pop_table, scenario_name, bias_corr):
+def distr_pop_bc(pop_table, scenario_name, bias_corr):
 
 
     years = np.unique(pop_table['Year'])
@@ -131,7 +168,7 @@ def distr_pop(pop_table, scenario_name, bias_corr):
         
         #adjust
         year_dat_merge = pd.merge(year_dat, bias_corr, on = 'Area')
-        year_dat_merge['Population_corr'] = year_dat_merge['Population'] - year_dat_merge['avg_dif']
+        year_dat_merge['Population_corr'] = year_dat_merge['Population'] + year_dat_merge['avg_dif']
         
         
         cnames = np.unique(year_dat['Area'])
@@ -163,8 +200,46 @@ def distr_pop(pop_table, scenario_name, bias_corr):
         pop_output.append(pop_year_frac)
     return pop_output
 
-pop_output = distr_pop(ssp2, 'ssp2', both_years)
+def distr_pop(pop_table, scenario_name):
 
+
+    years = np.unique(pop_table['Year'])
+    years = years[years >= 2000]
+    
+    pop_output = []
+    
+    for y in years:
+    
+        year_dat = pop_table[pop_table['Year'] == y]
+        cnames = np.unique(year_dat['Area'])
+        
+        pop_year = copy.deepcopy(countries_regrid)
+        
+        # get all country codes from map
+        dat = pop_year.data
+        vals = np.unique(dat)
+        vals = vals[vals.mask == False]
+        
+        for val in vals:
+            c_name = c_dict[val]
+            print(c_name)
+            
+            if c_name in cnames:
+        
+                replace_val = year_dat[year_dat['Area'] == c_name]['Population'].values
+                print(replace_val)
+                
+                pop_year.data = np.where(pop_year.data == val, replace_val, pop_year.data)
+                
+                pop_year_frac = copy.deepcopy(pop_year)
+                pop_year_frac.data = pop_year_frac.data*popfrac_2019.data
+            
+        
+        pop_output.append(pop_year_frac)
+    return pop_output
+
+pop_output = distr_pop_bc(ssp2, 'ssp2', both_years)
+pop_output_raw = distr_pop(ssp2, 'ssp2')
 
 
 #%%  check
@@ -172,6 +247,7 @@ pop_output = distr_pop(ssp2, 'ssp2', both_years)
 pop_levs = [0, 3000, 20000, 30000, 50000, 100000, 250000, 500000] 
 
 qplt.contourf(pop2019, levels = pop_levs, extend = 'max', cmap = 'YlOrRd')
+qplt.contourf(p2019_regrid, levels = pop_levs, extend = 'max', cmap = 'YlOrRd')
 qplt.contourf(pop_output[4], levels = pop_levs, extend = 'max', cmap = 'YlOrRd')
 
 
@@ -180,12 +256,17 @@ qplt.contourf(p2000_regrid, levels = pop_levs, extend = 'max', cmap = 'YlOrRd')
 
 #%%
 print(np.nanmean(pop2019.data))
+print(np.nanmean(p2019_regrid.data))
 mpop = [np.nanmean(x.data) for x in pop_output]
 #mpop3 = [np.nanmean(x.data) for x in pop_output_ssp3]
 
 
 print(np.nansum(pop2019.data))
+print(np.nansum(p2019_regrid.data))
+print(np.nansum(df_2019['pop']))
 tpop = [np.nansum(x.data) for x in pop_output]
+tpop_raw = [np.nansum(x.data) for x in pop_output_raw]
+
 #tpop3 = [np.nansum(x.data) for x in pop_output_ssp3]
 
 #%%
@@ -193,10 +274,14 @@ tpop = [np.nansum(x.data) for x in pop_output]
 years = np.unique(ssp2['Year'])
 years = years[years >= 2000]
 
-plt.plot(years, tpop, label = 'SSP2')
+plt.plot(years, tpop, label = 'SSP2 Corrected')
+plt.plot(years, tpop_raw, label = 'SSP2')
+
 #plt.plot(years, tpop3, label = 'SSP3')
-plt.scatter(2010, np.nansum(p2010_regrid.data), label = 'actual 2010')
-plt.scatter(2000, np.nansum(p2000_regrid.data), label = 'actual 2000')
+plt.scatter([2000, 2010], [np.nansum(p2000_regrid.data), np.nansum(p2010_regrid.data)], label = 'Actual')
 plt.xlabel('Year')
 plt.ylabel('Total African population (under 5)')
 plt.legend()
+
+plt.savefig('/nfs/see-fs-02_users/earsch/Documents/Leeds/Inputdata_SSP2_corrected.png',
+         bbox_inches = 'tight', pad_inches = 0.3)
